9b9ef680326e6702075c5cf796fd64cfbbaacf36
kato11144 <ts4.29r@gmail.com>
2025-01-04 02:20:43 +0900

    refactor main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,22 @@
+from modules.logger_manager import LoggerManager
 from modules.tmdb_client import TMDbClient
 from modules.dump_manager import DumpManager
-from modules.logger_manager import LoggerManager
-from config import TMDB_ACCOUNT_ID
+
+logger_manager = LoggerManager()
+tmdb_client = TMDbClient()
+dump_manager = DumpManager()
 
 def main():
-    logger = LoggerManager.setup_logger()
-
-    logger.info('Starting main function')
-
-    tmdb_client = TMDbClient()
-
     try:
-        tmdb_client.authenticate_user()
+        logger = logger_manager.setup_logger()
+        logger.info('Starting main function')
 
+        tmdb_client.authenticate_user()
         logger.info('User authenticated successfully')
 
-        movie_watchlist = tmdb_client.get_movie_watchlist(TMDB_ACCOUNT_ID)
-        tv_watchlist = tmdb_client.get_tv_watchlist(TMDB_ACCOUNT_ID)
-
-        dump_manager = DumpManager('watchlists.json')
+        movie_watchlist = tmdb_client.get_movie_watchlist()
+        tv_watchlist = tmdb_client.get_tv_watchlist()
         dump_manager.save_watchlists(movie_watchlist, tv_watchlist)
-
         logger.info('Watchlists have been saved successfully')
 
     except Exception as e:
