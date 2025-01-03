675c672ddb85e5d311a47e8c050b2a6a22c9c683
kato11144 <ts4.29r@gmail.com>
2025-01-04 01:58:49 +0900

    refactor logger_manager.py

--- app/modules/logger_manager.py
+++ app/modules/logger_manager.py
@@ -3,19 +3,19 @@ import os
 from logging.handlers import RotatingFileHandler
 
 class LoggerManager:
-    @staticmethod
-    def setup_logger(log_file='app.log', level=logging.DEBUG):
+    def __init__(self):
+        self.level = logging.DEBUG
+        self.dir_name = 'logs'
+        self.file_name = 'app.log'
+        self.file_path = os.path.join(self.dir_name, self.file_name)
+        os.makedirs(self.dir_name, exist_ok=True)
+
+    def setup_logger(self):
         logger = logging.getLogger()
-        logger.setLevel(level)
+        logger.setLevel(self.level)
 
-        log_dir = 'logs'
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-
-        log_path = os.path.join(log_dir, log_file)
-
-        handler = RotatingFileHandler(log_path, maxBytes=5*1024*1024, backupCount=2, encoding='utf-8')
-        handler.setLevel(level)
+        handler = RotatingFileHandler(self.file_path, maxBytes=5*1024*1024, backupCount=2, encoding='utf-8')
+        handler.setLevel(self.level)
 
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         handler.setFormatter(formatter)
@@ -24,7 +24,7 @@ class LoggerManager:
         logger.addHandler(logging.StreamHandler())
 
         urllib3_logger = logging.getLogger('urllib3')
-        urllib3_logger.setLevel(logging.DEBUG)
+        urllib3_logger.setLevel(self.level)
         urllib3_logger.addHandler(handler)
 
         return logger
